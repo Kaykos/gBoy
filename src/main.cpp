ede82c50db2b0252efc42a7dea50372bf9ee30ab
Sebastián Bobadilla <bobadillasebas@gmail.com>
2026-04-16 22:15:35 -0500

    Added event loop in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -31,7 +31,21 @@ int main(int argc, char* argv[]) {
 
     // Keep the window open for 3 seconds
     std::cout << "SDL3 successfully initialized. Window created!" << std::endl;
-    SDL_Delay(3000);
+
+
+    bool isRunning = true;
+    SDL_Event event;
+
+    // Will keep running until the window is closed
+    while (isRunning) {
+        // Poll for events from the OS
+        while (SDL_PollEvent(&event)) {
+            // If the user clicked the close button, break the loop
+            if (event.type == SDL_EVENT_QUIT) {
+                isRunning = false;
+            }
+        }
+    }
 
     // Clean up and exit
     SDL_DestroyWindow(window);
