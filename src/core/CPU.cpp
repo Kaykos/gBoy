b05f1cd984041408c493e00c3022fd5bcc6621f4
Sebastián Bobadilla <bobadillasebas@gmail.com>
2026-04-17 00:42:18 -0500

    Flag helpers

--- src/core/CPU.cpp
+++ src/core/CPU.cpp
@@ -22,17 +22,28 @@ namespace Core {
         a = (value & 0xFF00) >> 8;
         f = value & 0x00F0; // The bottom 4 bits of the F register are always zerp
     }
-    void CPU::set_bc(const uint16_t value) {
-        b = (value & 0xFF00) >> 8;
-        c = value & 0x00FF;
+    void CPU::set_bc(const uint16_t value) {b = (value & 0xFF00) >> 8; c = value & 0x00FF;}
+    void CPU::set_de(const uint16_t value) {d = (value & 0xFF00) >> 8; e = value & 0x00FF;}
+    void CPU::set_hl(const uint16_t value) {h = (value & 0xFF00) >> 8; l = value & 0x00FF;}
+
+    // Flag helpers
+    // If setting to 1, OR against the mask
+    // If setting to 0, AND against the inverse of the mask
+    void CPU::set_flag_z(const bool value) {
+        if (value) f |= FLAG_Z;
+        else f &= ~FLAG_Z;
     }
-    void CPU::set_de(const uint16_t value) {
-        d = (value & 0xFF00) >> 8;
-        e = value & 0x00FF;
+    void CPU::set_flag_n(const bool value) {
+        if (value) f |= FLAG_N;
+        else f &= ~FLAG_N;
     }
-    void CPU::set_hl(const uint16_t value) {
-        h = (value & 0xFF00) >> 8;
-        l = value & 0x00FF;
+    void CPU::set_flag_h(const bool value) {
+        if (value) f |= FLAG_H;
+        else f &= ~FLAG_H;
+    }
+    void CPU::set_flag_c(const bool value) {
+        if (value) f |= FLAG_C;
+        else f &= ~FLAG_C;
     }
 
     uint16_t CPU::fetch() {
--- src/core/CPU.h
+++ src/core/CPU.h
@@ -22,18 +22,18 @@ namespace Core {
     private:
         Bus* bus;
 
-        // 8-bit registers
+        // -- 8-bit registers --
         // These can be accessed as 16-bit pairs: AF, BC, DE, HL
         uint8_t a = 0, f = 0;
         uint8_t b = 0, c = 0;
         uint8_t d = 0, e = 0;
         uint8_t h = 0, l = 0;
 
-        // 16-bit registers
+        // -- 16-bit registers --
         uint16_t sp; // Stack pointer
         uint16_t pc; // Program counter
 
-        // Helpers for paired registers
+        // -- Helpers for paired registers --
         // [[nodiscard]] is used to make sure whenever the function is called, the return value is taken/assigned
         [[nodiscard]] uint16_t get_af() const;
         [[nodiscard]] uint16_t get_bc() const;
@@ -44,6 +44,23 @@ namespace Core {
         void set_bc(uint16_t value);
         void set_de(uint16_t value);
         void set_hl(uint16_t value);
+
+        // -- Flag helpers --
+        // Bit masks to extract the value of specific bits
+        static constexpr uint8_t FLAG_Z = 1 << 7; // Zero flag. Bit #7
+        static constexpr uint8_t FLAG_N = 1 << 6; // Substraction flag. Bit #6
+        static constexpr uint8_t FLAG_H = 1 << 5; // Half carry flag. Bit #5
+        static constexpr uint8_t FLAG_C = 1 << 4; // Carry flag. Bit #4
+
+        [[nodiscard]] bool get_flag_z() const {return f & FLAG_Z;}
+        [[nodiscard]] bool get_flag_n() const {return f & FLAG_N;}
+        [[nodiscard]] bool get_flag_h() const {return f & FLAG_H;}
+        [[nodiscard]] bool get_flag_c() const {return f & FLAG_C;}
+
+        void set_flag_z(bool value);
+        void set_flag_n(bool value);
+        void set_flag_h(bool value);
+        void set_flag_c(bool value);
 
         // Execution functions
         uint16_t fetch();
